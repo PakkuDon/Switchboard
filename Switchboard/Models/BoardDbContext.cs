9a336e6b2cfbe22453335995109be27d2e060951
PakkuDon <pakku.pd@gmail.com>
2015-09-10 14:59:27 +1000

    Initial commit

---
+++ Switchboard/Models/BoardDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Switchboard.Models
+{
+    public class BoardDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public BoardDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static BoardDbContext Create()
+        {
+            return new BoardDbContext();
+        }
+    }
+}
---
+++ Switchboard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Switchboard.Startup))]
+namespace Switchboard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
