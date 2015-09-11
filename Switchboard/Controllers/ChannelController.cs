0fbf209fed712061e3ff09c0ec9504bf6f11362f
PakkuDon <pakku.pd@gmail.com>
2015-09-11 15:45:03 +1000

    Added Channel/Index action and associated view
    - Created ChannelController.
    - Created child action Channel/Index - renders a list of available
      channels.
    - Added styling rules for channel list.
    - Minor: Case change in Home/About page.

---
+++ Switchboard/Controllers/ChannelController.cs
@@ -0,0 +1,26 @@
+﻿using Switchboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Switchboard.Controllers
+{
+    public class ChannelController : Controller
+    {
+        private BoardDbContext db = BoardDbContext.Create();
+
+        // GET: Channel
+        [ChildActionOnly]
+        public ActionResult Index()
+        {
+            return PartialView(db.Channels.OrderBy(c => c.Name).ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
