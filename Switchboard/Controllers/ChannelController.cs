ca5935afc55ab685136f2b566bfe79cc0dac44f5
PakkuDon <pakku.pd@gmail.com>
2015-09-12 23:23:46 +1000

    Implemented Channel/View action.

--- Switchboard/Controllers/ChannelController.cs
+++ Switchboard/Controllers/ChannelController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,6 +17,25 @@ namespace Switchboard.Controllers
         public ActionResult Index()
         {
             return PartialView(db.Channels.OrderBy(c => c.Name).ToList());
+        }
+
+        // GET: Channel/5
+        public ActionResult View(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            // Retrieve selected channel and eager load posts
+            var channel = db.Channels.Find(id);
+            if (channel == null)
+            {
+                return HttpNotFound();
+            }
+            db.Entry(channel).Collection(c => c.Posts);
+
+            return View(channel);
         }
 
         protected override void Dispose(bool disposing)
