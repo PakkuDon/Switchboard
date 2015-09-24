fc9aadd08215c6b7f7edb8bf5e7c824bd4597707
PakkuDon <pakku.pd@gmail.com>
2015-09-24 23:49:37 +1000

    Added an admin page; tidied some views
    - Created Admin area
    - Created index and main layout of admin area
    - Tidied Channel/Index view - whole view (ie: heading and list) is now
      included in relevant .cshtml file.
    - Simplified a few CSS selectors.
    - Added namespaces to route configuration.

---
+++ Switchboard/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Switchboard.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Actions()
+        {
+            return PartialView();
+        }
+    }
+}
