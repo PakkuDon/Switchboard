d6a09d3d85a262c7aeabbbbabcd4f6bb8156da05
PakkuDon <pakku.pd@gmail.com>
2015-10-07 19:01:44 +1100

    Added /Flag/Index page
    - Created FlagController.
    - Added /Flag/Index action and associated view.

---
+++ Switchboard/Controllers/FlagController.cs
@@ -0,0 +1,39 @@
+﻿using Switchboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+
+namespace Switchboard.Controllers
+{
+    [Authorize]
+    public class FlagController : Controller
+    {
+        private BoardDbContext db = BoardDbContext.Create();
+
+        // GET: Flag
+        public ActionResult Index()
+        {
+            // Retrieve flags created by current user
+            var flags = db.Flags
+                .Include(f => f.User)
+                .Include(f => f.Post)
+                .Include(f => f.Post.Channel)
+                .Where(f => f.User.Id == User.Identity.GetUserId());
+            return View(flags.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
