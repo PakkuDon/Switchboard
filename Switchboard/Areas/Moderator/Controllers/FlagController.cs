796f4741cccf6d6cfb1b89b48bee1354ff07f98d
PakkuDon <pakku.pd@gmail.com>
2015-10-26 23:48:03 +1100

    Added Moderator area
    - Scaffolded Moderator area.
    - Set default Moderator controller to Moderator/Flag.
    - Added FlagController and /Moderator/Flag/Index method.

---
+++ Switchboard/Areas/Moderator/Controllers/FlagController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Switchboard.Models;
+using System.Data.Entity;
+
+namespace Switchboard.Areas.Moderator.Controllers
+{
+    [Authorize(Roles = "Admin,Moderator")]
+    public class FlagController : Controller
+    {
+        private BoardDbContext db = BoardDbContext.Create();
+
+        // GET: Moderator/Flag
+        public ActionResult Index()
+        {
+            var flags = db.Flags
+                .Include(f => f.Post)
+                .Include(f => f.User)
+                .OrderByDescending(f => f.ReportedOn);
+            return View(flags.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
